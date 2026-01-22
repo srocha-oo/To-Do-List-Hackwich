d51c59d3d24be83735632bc71362afc54669c043
Stephanie Rocha M <srochamarquez@bsd220.org>
2026-01-22 10:24:08 -0600

    Initial Commit

---
+++ To-Do List Hackwich/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  To-Do List Hackwich
+//
+//  Created by stephanie rocha marquez on 1/22/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ To-Do List Hackwich/To_Do_List_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_List_HackwichApp.swift
+//  To-Do List Hackwich
+//
+//  Created by stephanie rocha marquez on 1/22/26.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_List_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
